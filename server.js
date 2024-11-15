2d3db488d34be6058437898c1e7d50587d2ca065
Rodrigo Immaginario <rodrigoi@hotmail.com>
2024-11-15 15:56:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ async function sendEmail(ponto) {
 
         const mailOptions = {
             from: process.env.EMAIL_USER || 'MonitorBueiros@gmail.com',
-            to: 'MonitorBueiros@gmail.com', // Substitua pelo destinatário
+            to: 'luana.immaginario@davincionline.com.br', // Substitua pelo destinatário
             subject: `Alerta: Ponto Marcado`,
             text: `O ponto "${ponto.nome}" foi marcado como ativo. Endereço: ${ponto.endereco}`
         };
